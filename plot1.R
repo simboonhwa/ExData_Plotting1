2dc04592c539141ad24aeb282258260b3eaadfd6
Sim Boon Hwa <simboonhwa@localhost.localdomain>
2015-03-31 00:05:21 +0800

    update

--- plot1.R
+++ plot1.R
@@ -1,15 +1,27 @@
 power <- read.table("./household_power_consumption.txt",skip=21997,nrows=525600,
 		sep=";",header=F,stringsAsFactors = FALSE,comment.char="", 
 		na.strings="?", 
-		colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+		colClasses=c("character","character","numeric","numeric",
+			"numeric","numeric","numeric","numeric","numeric"))
 
-names(power) <- c("Date","Time","active_power","reactive_power","Voltage","intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+names(power) <- c("Date","Time","active_power","reactive_power",
+		"Voltage","intensity","Sub_metering_1","Sub_metering_2",
+		"Sub_metering_3")
 
-X11()
-hist(power$active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red")
-dev.copy(png,filename="plot1.png")
-dev.off()
-qplot(wday(power$Date,label=T),power$active_power,data=power,xlab="x",ylab="y")
+#X11()
+#hist(power$active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red")
+#dev.copy(png,filename="plot1.png")
+
+strptime(paste(power$Date,power$Time), format="%y-%m-%d, %I:%M%p")
+strptime(power$Date, format="%y-%m-%d")
+library(ggplot2)
+library(lubridate)
+#qplot(wday(power$Date,label=T),power$active_power,data=power,xlab="x",ylab="y",
+extract <- within(power,{Date<-wday(Date,label=T)
+rm(reactive_power,Voltage,intensity,Sub_metering_1,Sub_metering_2,Sub_metering_3)})
+qplot(paste(extract$Date,extract$Time), extract$active_power, data=extract,
+	xlab="x", ylab="y", geom="line")
+#dev.off()
 #cut2(as.double(power$active_power),g=12)
 #strptime(x, format="%y-%m-%d, %I:%M%p")
 #x[x %in% c(98,99)] = NA
