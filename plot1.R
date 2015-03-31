1346a14fe8e0af2e0ac6091fe52b8af19f10b2cc
Sim Boon Hwa <simboonhwa@localhost.localdomain>
2015-04-01 07:40:10 +0800

    update

--- plot1.R
+++ plot1.R
@@ -9,18 +9,27 @@ names(power) <- c("Date","Time","active_power","reactive_power",
 		"Sub_metering_3")
 
 #X11()
-#hist(power$active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red")
+hist(power$active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",ylab="Frequency",col="red")
 #dev.copy(png,filename="plot1.png")
 
-strptime(paste(power$Date,power$Time), format="%y-%m-%d, %I:%M%p")
-strptime(power$Date, format="%y-%m-%d")
-library(ggplot2)
 library(lubridate)
+library(ggplot2)
+#strptime(power$Date, format="%y-%m-%d")
+power$Date <- wday(power$Date,label=T) 
+qplot(paste(Date,Time), active_power, data=power, ylab="Global Active Power (kilowatts)", geom="line")
+
+ggplot(power, aes(paste(Date,Time)),Sub_metering_1) + geom_density() + 
+	color="grey0" + ylab("Energg Sub Metering")+ 
+	guide_legend(title="Sub_metering_1")
+qplot(paste(Date,Time), Sub_metering_1, data=power, ylab="Energy Sub Metering",
+ geom="line", color="grey0")#,label="Sub_metering_1")
+qplot(paste(Date,Time), Sub_metering_2, data=power, ylab="Energy Sub Metering",
+ geom="line", color="red")#,label="Sub_metering_2")
+
+strptime(paste(power$Date,power$Time), format="%y-%m-%d, %I:%M%p")
 #qplot(wday(power$Date,label=T),power$active_power,data=power,xlab="x",ylab="y",
-extract <- within(power,{Date<-wday(Date,label=T)
-rm(reactive_power,Voltage,intensity,Sub_metering_1,Sub_metering_2,Sub_metering_3)})
-qplot(paste(extract$Date,extract$Time), extract$active_power, data=extract,
-	xlab="x", ylab="y", geom="line")
+#extract <- within(power,{Date<-wday(Date,label=T)
+#rm(reactive_power,Voltage,intensity,Sub_metering_1,Sub_metering_2,Sub_metering_3)})
 #dev.off()
 #cut2(as.double(power$active_power),g=12)
 #strptime(x, format="%y-%m-%d, %I:%M%p")
